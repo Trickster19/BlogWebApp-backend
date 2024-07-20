b3fe0fbe43da1f734ee7cb145e72b5a5db321424
Trickster19 <harrismohd786@gmail.com>
2024-07-20 13:21:26 +0530

    utils added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./env"
+});
 import express from "express";
-
-
+import cors from "cors"
+import cookieparser from "cookie-parser"
 
 const app = express();
 
+app.use(cors({
 
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:'16kb'}))
+app.get("/",(req,res)=>{
+    res.json({"messg":'hello Cutie'})
+})
 export default app;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ dotenv.config({
     path: "./env"
 });
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
+connectDB()
+.then(()=>{
+    const PORT=process.env.PORT||8000;
 
-connectDB();
+    app.on("error",(err)=>{
+        console.log("ERROR : ",err);
+        throw err;
+    })
+    
+    app.listen(PORT,()=>{
+
+        console.log(`Server is running at PORT ${PORT}`);
+        
+    })
+});
